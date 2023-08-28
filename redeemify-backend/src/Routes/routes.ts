0d1a1e5b417270d0e0d7399f4bbda3e85046f6de
Prathamesh <prathameshmane47@gmail.com>
2023-08-28 15:24:06 +0530

    conflict fix

--- redeemify-backend/src/Routes/routes.ts
+++ redeemify-backend/src/Routes/routes.ts
@@ -1,17 +1,19 @@
 import express from 'express';
-import {Request, Response} from 'express' ;
+import { Request, Response } from 'express';
 import productsRouter from './product.routes';
 import userRouter from './user.routes';
 import authRouter from './auth.routes';
+import WalletRouter from './wallet.routes';
 
 const router = express.Router();
 
-router.get('', (req: Request,res: Response) => {
-  res.send("hello");  
+router.get('', (req: Request, res: Response) => {
+  res.send("hello");
 })
 
 // router.use('/api/auth', authRouter);
 router.use('/api/products', productsRouter);
-// router.use('/api/users', userRouter);
+router.use('/api/users', userRouter);
+router.use('/api/wallet', WalletRouter);
 
 export default router;
