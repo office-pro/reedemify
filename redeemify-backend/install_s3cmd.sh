4318c95be488a6b864294c34fcf8303ba8af8dff
Shashikant Mittapelli <shaelm29@gmail.com>
2023-09-25 19:46:10 +0530

    added changes

--- redeemify-backend/install_s3cmd.sh
+++ redeemify-backend/install_s3cmd.sh
@@ -1,46 +1,33 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Function to install s3cmd
-# install_s3cmd() {
-#     # Ensure that pip is installed
-#     if ! command -v pip &> /dev/null; then
-#         echo "pip is not installed. Please install pip before proceeding."
-#         exit 1
-#     fi
+# Install s3cmd using pip
+echo "Installing s3cmd..."
+pip install s3cmd
 
-#     # Install s3cmd using pip
-#     pip install s3cmd
+# Verify the installation
+if ! command -v s3cmd &> /dev/null; then
+    echo "s3cmd installation failed. Please check for errors."
+    exit 1
+fi
 
-#     # Verify the installation
-#     if command -v s3cmd &> /dev/null; then
-#         echo "s3cmd installed successfully."
-#     else
-#         echo "s3cmd installation failed. Please check for errors."
-#     fi
-# }
+# Determine the directory containing the s3cmd executable
+s3cmd_dir=$(dirname $(which s3cmd))
 
-# # Function to add s3cmd to PATH
-# add_s3cmd_to_path() {
-#     # Locate the s3cmd executable
-#     local s3cmd_path
-#     s3cmd_path=$(which s3cmd)
+# Check if the s3cmd directory is already in the PATH
+if [[ ":$PATH:" != *":$s3cmd_dir:"* ]]; then
+    # If not, add it to the PATH
+    echo "Adding s3cmd directory to PATH..."
+    echo "export PATH=\$PATH:$s3cmd_dir" >> ~/.bashrc
+    source ~/.bashrc
+    echo "s3cmd directory added to PATH."
+else
+    echo "s3cmd directory is already in PATH."
+fi
 
-#     if [ -n "$s3cmd_path" ]; then
-#         # Add s3cmd to PATH
-#         export PATH="$PATH:$s3cmd_path"
-#         echo "s3cmd added to PATH."
-#     else
-#         echo "s3cmd executable not found. Please install s3cmd first."
-#     fi
-# }
-
-# # Install s3cmd
-# install_s3cmd
-
-# # Add s3cmd to PATH
-# add_s3cmd_to_path
-
-
-sudo apt-get install s3cmd
-
-s3cmd ls
+# Verify the s3cmd command again
+if command -v s3cmd &> /dev/null; then
+    echo "s3cmd installed successfully."
+    echo "You can now use the 's3cmd' command."
+else
+    echo "s3cmd installation or PATH configuration encountered issues."
+fi
