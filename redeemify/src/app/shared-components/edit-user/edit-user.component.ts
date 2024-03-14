98d23707f7671bde73f529dcb83753ff11338765
poonamchaursia1998@gamil.com <poonamchaursia1998@gamil.com>
2024-03-14 13:45:57 +0530

    editUser data-bind

--- redeemify/src/app/shared-components/edit-user/edit-user.component.ts
+++ redeemify/src/app/shared-components/edit-user/edit-user.component.ts
@@ -1,7 +1,6 @@
-import { Component, Inject } from "@angular/core";
-import {MAT_DIALOG_DATA, MatDialogModule} from "@angular/material/dialog";
-import {MatButtonModule} from "@angular/material/button"
-import { FormBuilder } from "@angular/forms";
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'edit-user',
@@ -9,21 +8,34 @@ import { FormBuilder } from "@angular/forms";
   styleUrls:['./edit-user.component.scss'],
 })
 
-export class EditUserComponent {
-
+export class EditUserComponent implements OnInit {
   brandData: Array<any> = [];
   roleData: Array<any> = [];
   brandId: number = 0;
-
   brandName: string = "";
   roleName: string = "";
 
-  editUserForm: any;
+  editUserForm!: FormGroup; // Define the FormGroup
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder) {}
 
-  onSelectedBrandChange(event: any) {}
+  ngOnInit(): void {
+    // Initialize the FormGroup
+    this.editUserForm = this.fb.group({
+      firstName: [this.data.firstName, Validators.required],
+      lastName: [this.data.lastName, Validators.required],
+      brandId: [this.data.brandId, Validators.required],
+      roleId: [this.data.roleId, Validators.required],
+      mobileNo: [this.data.mobileNo, Validators.required],
+      email: [this.data.email, [Validators.required, Validators.email]]
+    });
+  }
 
-  onSelectedRoleChange(event: any) {}
+  onSelectedBrandChange(event: any) {
+    // Implement your logic for brand change if needed
+  }
 
+  onSelectedRoleChange(event: any) {
+    // Implement your logic for role change if needed
+  }
 }
